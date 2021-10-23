51f441bd7a7a1c0df3561a5dbf5857ce7acab5d7
dev008abhi <thisdevabhinav@gmail.com>
2021-10-24 02:44:54 +0530

    A basic event emiter example

---
+++ basicEventEmitter/eventEmitter.js
@@ -0,0 +1,9 @@
+const eventEmitter = require("events");
+class exampleEventEmitter extends eventEmitter{}
+
+const exampleEmitter = new exampleEventEmitter();
+exampleEmitter.on('event',()=>{
+  console.log('an event occured');
+});
+
+exampleEmitter.emit('event');
